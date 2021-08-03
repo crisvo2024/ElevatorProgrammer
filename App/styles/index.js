aeb152d2d2b877c05fe0166e61809a9460ba8857
Santiago <crvargaso@unal.edu.co>
2021-08-02 19:29:58 -0500

    init

---
+++ App/styles/index.js
@@ -0,0 +1,12 @@
+// 1. Import the extendTheme function
+import {extendTheme} from 'native-base';
+// 2. Extend the theme to include custom colors, fonts, etc
+const newColorTheme = {
+  brand: {
+    900: '#8287af',
+    800: '#7c83db',
+    700: '#b3bef6',
+  },
+};
+const theme = extendTheme({colors: newColorTheme});
+export default theme;
